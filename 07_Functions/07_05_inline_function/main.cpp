4cd17e539935aee5a60c2d4c6e5c9cb8c6407949
yu nie <nieyuchina@gmail.com>
2020-09-30 07:48:01 +0800

    add inline overloading

---
+++ 07_Functions/07_05_inline_function/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  07_05_inline_function
+//
+//  Created by nie yu on 2020/9/29.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    
+    
+    return 0;
+}
---
+++ 07_Functions/07_06_function_overloading/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  07_06_function_overloading
+//
+//  Created by nie yu on 2020/9/29.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
