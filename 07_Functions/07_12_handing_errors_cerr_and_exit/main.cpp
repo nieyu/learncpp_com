86523b9731f29dab3bda0a36a428fff899b8666f
nieyu-txc-mini01 <nieyuchina@163.com>
2020-10-19 20:04:44 +0800

    add function

---
+++ 07_Functions/07_12_handing_errors_cerr_and_exit/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  07_12_handing_errors_cerr_and_exit
+//
+//  Created by txc-ios on 2020/10/19.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    
+    
+    return 0;
+}
