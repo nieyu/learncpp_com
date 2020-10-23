c6baca634883a8c3feb818a1ca12e50c0a01ec40
nieyu-txc-mini01 <nieyuchina@163.com>
2020-10-23 21:12:42 +0800

    add 08_deconstructor

--- 08_basic_object_oriented_programming/08_07_desctructors/main.cpp
+++ 08_basic_object_oriented_programming/08_07_desctructors/main.cpp
@@ -6,7 +6,25 @@
 //
 
 #include <iostream>
+#include <cassert>
+#include <cstddef>
 
+class IntArray {
+private:
+    int *m_array{};
+    int m_length{};
+public:
+    IntArray(int length) {
+        assert(length > 0);
+        m_array = new int[static_cast<std::size_t>(length)];
+        m_length = length;
+    }
+    ~IntArray() {
+        delete[] m_array;
+    }
+    void setValue(int index, int value) { m_array[index] = value; }
+    int getValue(int index) { return m_array[index]; }
+};
 
 int main(int argc, const char * argv[]) {
     
